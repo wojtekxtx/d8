4970f88a730bcd74b9b6b7722952e3bfa051ae03
wojtekxtx <wojtekx.tx@gmail.com>
2022-11-19 20:56:53 +0100

    set correct logic

--- js/pexp-draft.js
+++ js/pexp-draft.js
@@ -24,6 +24,22 @@ if (passChars < minPasswordLength) {
 
     // Sanitize password
     sanitize(pass);
+
+    // set expiration date
+    setPwdExpirationDate(pass);
+}
+
+function setPwdExpirationDate(string){
+
+    // Calculate PED
+    let d = new Date();
+    let ped = d.setDate(d.getDate()+10);
+
+    // Instantise Password Managerial Unit (PMU)
+    pm = new passwordMngmt();
+
+    // Set desired PED
+    pm.PasswordExpirationDate(ped);
 }
 
 // Sanitize password
