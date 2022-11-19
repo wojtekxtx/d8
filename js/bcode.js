766042a5af685e94722f32e84538e86d61be2843
wojtekxtx <wojtekx.tx@gmail.com>
2022-11-19 12:29:50 +0100

    add things

--- js/bcode.js
+++ js/bcode.js
@@ -1,9 +1,11 @@
 // Created with VSCode for Web inside GitHub CodeSpace
-// Created by: wojtekxtx
+// Created by: wojtekxtx.
+// NO COPIES/MODIFICATIONS ALLOWED
 
 class GenericBarcode extends Barcode{
 	constructor(data, options){
 		super(data, options); // Sets this.data and this.text
+		console.log("Data: "+data+" options: " +options);
 	}
 
 	// Return the corresponding binary numbers for the data provided
